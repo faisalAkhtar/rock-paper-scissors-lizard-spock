52b49a379da012b540591218dc9c68370260038e
Faisal Akhtar PC <faisalakhtar1999@gmail.com>
2020-08-25 18:01:14 +0530

    Updates

---
+++ precache-manifest.f998eed1e1cc1529251e93b7c458fa5c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48764f02a34adc56a2525fc89964a5b2",
+    "url": "/rock-paper-scissor-lizard-spock/index.html"
+  },
+  {
+    "revision": "242b1af75abe3263c7e8",
+    "url": "/rock-paper-scissor-lizard-spock/static/css/main.2cb4bf58.chunk.css"
+  },
+  {
+    "revision": "8e051af17d92c79568c0",
+    "url": "/rock-paper-scissor-lizard-spock/static/js/2.d1f1c3d5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rock-paper-scissor-lizard-spock/static/js/2.d1f1c3d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "242b1af75abe3263c7e8",
+    "url": "/rock-paper-scissor-lizard-spock/static/js/main.a532a961.chunk.js"
+  },
+  {
+    "revision": "1cf54311154b06c81955",
+    "url": "/rock-paper-scissor-lizard-spock/static/js/runtime-main.a26c6b4b.js"
+  }
+]);
