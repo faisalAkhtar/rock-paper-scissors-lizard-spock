a7f2fcb2cc09d562f8e63237588cf29c6141914d
Faisal Akhtar PC <faisalakhtar1999@gmail.com>
2020-08-26 07:19:30 +0530

    ‎

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './index.css';
+import App from './App.jsx';
+
+function Check(props) {
+    return (
+        < App />
+    )
+}
+
+ReactDOM.render(
+    <Check />,
+    document.getElementById('root')
+);
